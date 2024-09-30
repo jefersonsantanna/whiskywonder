24b063d350d0e88888a9298d14324d58a38c9af9
Jeferson Sant'Anna <seu-email@example.comclear>
2024-09-30 18:20:38 -0300

    Commit Inicial

---
+++ src/main/java/com/jeferson/licitPro/LicitProApplication.java
@@ -0,0 +1,13 @@
+package com.jeferson.licitPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicitProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicitProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeferson/licitPro/LicitProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeferson.licitPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicitProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
